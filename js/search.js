4ebce308a9b15162f11795f295114738eb739fc2
agenaro12 <124233810+agenaro12@users.noreply.github.com>
2025-03-19 22:04:57 -0700

    Changes to design to index.html

--- js/search.js
+++ js/search.js
@@ -1,11 +1,14 @@
 const Books = [
-    {id:1,name:"The Fellowship Of The Ring",price:20},
-    {id:2,name:"The Two Towers",price:18},
-    {id:3,name:"Return of the King",price:30},
-    {id:4,name:"Aventures of Robinson Crusoe",price:20},
-    {id:5,name:"Song of Ice and Fire",price:17},
-    {id:6,name:"The Bones Collector",price:16},
-    {id:7,name:"Around the world in 80 days",price:22},
+    {id:1,name:"The Fellowship Of The Ring",price:20.00},
+    {id:2,name:"The Two Towers",price:18.00},
+    {id:3,name:"Return of the King",price:30.00},
+    {id:4,name:"Aventures of Robinson Crusoe",price:20.00},
+    {id:5,name:"Song of Ice and Fire",price:17.00},
+    {id:6,name:"The Bones Collector",price:16.00},
+    {id:7,name:"Around the world in 80 days",price:22.00},
+    {id:8,name:"The Simmarilion",price:35.00},
+    {id:9,name:"Unfinished Tales of Tolkien",price:28.00},
+    {id:10,name:"League of Extraordinary Men",price:15.00},
 ];
 
 const Search = (prompt) => {
@@ -21,7 +24,7 @@ const bookTable = (tableClass) => {
     let table = `<table class='${tableClass}'>`;
     table += `<tr><th class="bookTable_th">Id</th><th class="bookTable_th">Name</th><th class="bookTable_th">Price</th></tr>`;
     Books.forEach((value)=>{
-        table+=`<tr><td class="bookTable_td">${value.id}</td><td class="bookTable_td">${value.name}</td><td class="bookTable_td">${value.price}</td></tr>`;
+        table+=`<tr><td class="bookTable_td">${value.id}</td><td class="bookTable_td"><a href="#" class="book_link">${value.name}</a></td><td class="bookTable_td">$${value.price.toFixed(2)}</td></tr>`;
     });
     table += "</table>";
     document.getElementById('libraryCnt').innerHTML=table;
